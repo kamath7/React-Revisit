31ab3371c282f2c9f226637449f5243c18982c41
kamath7 <adithyakamath96@gmail.com>
2021-05-13 20:23:13 +0530

    Created first component with styling

--- Basics/basic-guide/src/App.js
+++ Basics/basic-guide/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
     <div className="App">
         <h1>Hello Kams!</h1>
+        <p>This is also visible</p>
+    <ExpenseItem/>
     </div>
   );
 }
