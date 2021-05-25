e591967e3e3327b8c98a4d1f247f142beefa2b24
kamath7 <adithyakamath96@gmail.com>
2021-05-25 19:32:38 +0530

    Just starting with state

--- Basics/basic-guide/src/components/Expenses/ExpenseItem.js
+++ Basics/basic-guide/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-import './ExpenseItem.css'
-function ExpenseItem({expense}) {
-    // const expenseDate= new Date(2021,5,13)
-    // const expenseTitle = 'Hotel'
-    // const expenseAmount = 234.50
-
+import "./ExpenseItem.css";
+function ExpenseItem({ expense }) {
+  // const expenseDate= new Date(2021,5,13)
+  // const expenseTitle = 'Hotel'
+  // const expenseAmount = 234.50
+  const sayHi = () => {
+    alert("Hi!");
+  };
   return (
     <Card className="expense-item">
-      <div><ExpenseDate expense={expense}/></div>
+      <div>
+        <ExpenseDate expense={expense} />
+      </div>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">{expense.amount}</div>
       </div>
+      <button onClick={sayHi}>Change</button> {/* For state purposes */}
     </Card>
   );
 }
