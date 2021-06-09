e05721647729596f3b631002ee51e61ebff49fb5
kamath7 <adithyakamath96@gmail.com>
2021-06-09 22:23:52 +0530

    Change handlers added

--- practice-project-1/src/components/UI/Button.js
+++ practice-project-1/src/components/UI/Button.js
@@ -1,5 +1,6 @@
-import classes from "Button.module.css";
+
 import React from "react";
+import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
--- practice-project-1/src/components/Users/AddUser.js
+++ practice-project-1/src/components/Users/AddUser.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 const AddUser = () => {
+   const [enteredUsername, setUsername] = useState('') 
+   const [age, setAge] = useState('') 
+
   const adduserHandler = (e) => {
     e.preventDefault();
+    alert(`Entered value is ${enteredUsername} ${age}`)
   };
+  const userNameChangeHandler = (e)=>{
+      setUsername(e.target.value);
+  }
+  const ageChangeHandler = (e)=>{
+      setAge(e.target.value);
+  }
   return (
     <Card className={classes.input}>
       <form onSubmit={adduserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" placeholder="kams69" />
+        <input id="username" type="text" placeholder="kams69" value={enteredUsername} onChange={userNameChangeHandler}/>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" placeholder="25" />
+        <input id="age" type="number" placeholder="25" value={age} onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
