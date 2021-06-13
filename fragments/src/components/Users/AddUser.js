57cab359c95ad65ab7c598c311a1c353f3b9f991
kamath7 <adithyakamath96@gmail.com>
2021-06-14 01:49:33 +0530

    Refs added

--- fragments/src/components/Users/AddUser.js
+++ fragments/src/components/Users/AddUser.js
@@ -1,24 +1,29 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [enteredUsername, setUsername] = useState("");
   const [error, setError] = useState();
   const [age, setAge] = useState("");
 
   const adduserHandler = (e) => {
     e.preventDefault();
-    if (enteredUsername.trim().length === 0 || age.trim().length === 0) {
+    const enteredUserName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "Enter a valid name and age",
       });
       return;
     }
-    if (+age < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Enter an age > 0",
@@ -54,6 +59,7 @@ const AddUser = (props) => {
             placeholder="kams69"
             value={enteredUsername}
             onChange={userNameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age</label>
           <input
@@ -62,6 +68,7 @@ const AddUser = (props) => {
             placeholder="25"
             value={age}
             onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
