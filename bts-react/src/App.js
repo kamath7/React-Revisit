fb62807f6fdd949449f66424ea2329b0a47a4ed9
kamath7 <adithyakamath96@gmail.com>
2021-07-17 23:54:06 +0530

    Adding random text

--- bts-react/src/App.js
+++ bts-react/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showP, setShowP] = useState(false);
+  const toggleP = () => {
+    setShowP(prevShowP => !prevShowP);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi Kams!</h1>
+      <button onClick={toggleP}>Toggle  p</button>
+      {showP && <p>Some lalle text</p>}
+      {showP && <p> United title winners 2021-22 </p>}
     </div>
   );
 }
