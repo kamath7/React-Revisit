bd3b62e5a607a45b95cd185c7e3c07590010df9a
kamath7 <adithyakamath96@gmail.com>
2021-07-19 00:33:10 +0530

    Using useCallback

--- bts-react/src/App.js
+++ bts-react/src/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
-import DemoOutput from './components/Demo/DemoOutput'
+import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showP, setShowP] = useState(false);
-  const toggleP = () => {
-    setShowP(prevShowP => !prevShowP);
-  };
+  const toggleP = useCallback(() => {
+    setShowP((prevShowP) => !prevShowP);
+  },[]);
   return (
     <div className="App">
       <h1>Hi Kams!</h1>
-      <button onClick={toggleP}>Toggle  p</button>
-       <DemoOutput show={showP}/>
+      <button onClick={toggleP}>Toggle p</button>
+      <DemoOutput show={showP} />
       {showP && <p> United title winners 2021-22 </p>}
     </div>
   );
