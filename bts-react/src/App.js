7e6df9a5bafdd42462f65c667841a5987621c914
kamath7 <adithyakamath96@gmail.com>
2021-07-19 02:32:11 +0530

    playing with callback

--- bts-react/src/App.js
+++ bts-react/src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
   const toggleP = useCallback(() => {
-    setShowP((prevShowP) => !prevShowP);
-  },[]);
+    if (allowToggle) {
+      setShowP((prevShowP) => !prevShowP);
+    }
+  }, [allowToggle]);
+  const allowTogglep = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="App">
       <h1>Hi Kams!</h1>
+      <button onClick={allowToggle}>Allow Toggle p</button>
+
       <button onClick={toggleP}>Toggle p</button>
       <DemoOutput show={showP} />
       {showP && <p> United title winners 2021-22 </p>}
