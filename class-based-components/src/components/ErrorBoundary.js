d3a2e55ddb6a5135a52671a326ab2d72f2e6417e
kamath7 <adithyakamath96@gmail.com>
2021-08-10 02:25:27 +0530

    Error boundaries done

--- class-based-components/src/components/ErrorBoundary.js
+++ class-based-components/src/components/ErrorBoundary.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
 export default class ErrorBoundary extends Component {
-  componentDidCatch() {}
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
+  componentDidCatch(error) {
+    console.log(error);
+    this.setState({ hasError: true });
+  }
   render() {
+    if (this.state.hasError) {
+      <p>Something went wrong!</p>;
+    }
     return this.props.children;
   }
 }
