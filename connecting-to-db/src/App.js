c77566a77b2ee154b125cb623c6fc0e439ce18e5
kamath7 <adithyakamath96@gmail.com>
2021-08-11 01:03:18 +0530

    using POST with fetch

--- connecting-to-db/src/App.js
+++ connecting-to-db/src/App.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -11,45 +11,55 @@ function App() {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("someapi");
       const data = await response.json();
 
       if (!response.ok) {
         throw new Error("Something is wrong");
       }
-      const transforme = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-        };
-      });
-      setMovies(transforme);
+      const loadedMovies = []
+
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      // const transforme = data.results.map((movie) => {
+      //   return {
+      //     id: movie.episode_id,
+      //     title: movie.title,
+      //     openingText: movie.opening_crawl,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setLoading(false);
-    // fetch("https://swapi.dev/api/films/")
-    //   .then((res) => {
-    //     return res.json();
-    //   })
-    //   .then((data) => {
-    //     const transforme= data.results.map((movie)=> {
-    //       return {
-    //         id: movie.episode_id,
-    //         title: movie.title,
-    //         openingText: movie.opening_crawl
-    //       }
-    //     })
-    //     setMovies(transforme);
-    //   });
   }, []);
+  async function addMovieHandler(movie) {
+    const res = await fetch("someapi", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json()
+    console.log(data)
+  }
   useEffect(() => {
     fetchMovies();
   }, [fetchMovies]);
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
@@ -64,3 +74,17 @@ function App() {
 }
 
 export default App;
+// fetch("https://swapi.dev/api/films/")
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     const transforme= data.results.map((movie)=> {
+//       return {
+//         id: movie.episode_id,
+//         title: movie.title,
+//         openingText: movie.opening_crawl
+//       }
+//     })
+//     setMovies(transforme);
+//   });
