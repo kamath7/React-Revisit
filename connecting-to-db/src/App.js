e11ddbb120fabf5464b995e2452bd77109e20889
kamath7 <adithyakamath96@gmail.com>
2021-08-10 20:02:36 +0530

    Getting starwars movies

--- connecting-to-db/src/App.js
+++ connecting-to-db/src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  function fetchMovies() {
+    fetch("https://swapi.dev/api/films/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const transforme= data.results.map((movie)=> {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl
+          }
+        })
+        setMovies(transforme);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
