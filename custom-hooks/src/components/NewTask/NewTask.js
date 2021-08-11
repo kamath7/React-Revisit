68b65c8f544513ac1c2a7491bceaedaf2b24e6a5
kamath7 <adithyakamath96@gmail.com>
2021-08-12 01:42:49 +0530

    Pushing data

--- custom-hooks/src/components/NewTask/NewTask.js
+++ custom-hooks/src/components/NewTask/NewTask.js
@@ -1,41 +1,32 @@
-import { useState } from 'react';
+import { useState } from "react";
+import useHttp from "../../hooks/useHttp";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+  const { isLoading, error, sendReq } = useHttp(createTask);
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://lalle-react-app-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+    sendReq({
+      url: "https://lalle-react-app-default-rtdb.firebaseio.com/tasks.json",
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+      method: "POST",
+      body: JSON.stringify({ text: taskText }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        text: taskText,
+      },
+    });
+    createTask.bind(null, taskText);
   };
 
   return (
