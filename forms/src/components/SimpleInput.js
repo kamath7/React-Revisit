86c92828cf4165b5ba330244dbdf9568d7996ac3
kamath7 <adithyakamath96@gmail.com>
2021-08-13 01:19:59 +0530

    Adding css for form invalidation

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -2,24 +2,29 @@ import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const [name, setName] = useState("");
-  
+  const [nameValid, setNameValid] = useState(true);
   const nameInputRef = useRef(); //alternate approach
   const nameInputChangeHandler = (event) => {
     setName(event.target.value);
   };
   const formSubmit = (event) => {
     event.preventDefault();
-    if(name.trim() == ''){
+    if (name.trim() == "") {
+      setNameValid(false);
       return;
     }
+    setNameValid(true);
     console.log(name);
     const enteredVal = nameInputRef.current.value;
     console.log(`From ref ${enteredVal}`);
     setName("");
   };
+
+  
+  const nameInputClasses = nameValid ? "form-control" : "form-control-invalid";
   return (
     <form onSubmit={formSubmit}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -28,6 +33,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={name}
         />
+        {!nameValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
