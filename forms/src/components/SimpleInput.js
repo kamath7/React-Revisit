f83a1d29585c4d35102b0c951356a8a6d3d7d1b1
kamath7 <adithyakamath96@gmail.com>
2021-08-13 01:11:53 +0530

    Form handling

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+  const nameInputRef = useRef(); //alternate approach
+  const nameInputChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+  const formSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    const enteredVal = nameInputRef.current.value;
+    console.log(`From ref ${enteredVal}`);
+    setName("");
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmit}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={name}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
