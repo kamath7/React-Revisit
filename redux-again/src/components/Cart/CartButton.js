616f97d1f1f21dcde6dd5ebd4a4cf39931aa76c7
kamath7 <adithyakamath96@gmail.com>
2021-09-06 17:18:28 +0530

    ui toggling

--- redux-again/src/components/Cart/CartButton.js
+++ redux-again/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
-import classes from './CartButton.module.css';
+import { uiActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCart = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
