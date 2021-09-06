4d1ff7b17051cf004b0bc8d8a5f1a08b89f454ad
kamath7 <adithyakamath96@gmail.com>
2021-09-06 17:32:15 +0530

    addition of new items reducer

--- redux-again/src/store/ui-slice.js
+++ redux-again/src/store/ui-slice.js
@@ -1,13 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 createSlice({
-    name: 'cart',
-    initialState: {
-        items: [],
-        totalQuantity: 0,
-        // totalAmount:0
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    // totalAmount:0
+  },
+  reducers: {
+    addItemsToCart(state, action) {
+      const newItem = action.payload;
+      const existing = state.items.find((item) => item.id === newItem.id);
+      if (!existing) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existing.quantity++;
+        existing.totalPrice = +newItem.price;
+      }
     },
-    reducers:{
-        
-    }
-})
+    removeItemsFromCart(state) {},
+  },
+});
