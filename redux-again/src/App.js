3cb0310cb57876a862b32de2e3b2646d1b02c8a1
kamath7 <adithyakamath96@gmail.com>
2021-09-09 01:09:44 +0530

    useEffect to fetch

--- redux-again/src/App.js
+++ redux-again/src/App.js
@@ -5,7 +5,7 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 // import { uiActions } from "./store/cart-slice";
 import Notification from "./components/UI/Notification";
-import {sendCartData} from './store/cart-actions'
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 let isInit = true;
 function App() {
   const dispatch = useDispatch();
@@ -13,11 +13,14 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+  useEffect(() => {
     if (isInit) {
       isInit = false;
       return;
     }
-    dispatch(sendCartData(cart))
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <Fragment>
