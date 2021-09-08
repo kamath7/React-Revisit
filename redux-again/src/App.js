efa68bb8c873e5e3436952ba87128e803f2cd198
kamath7 <adithyakamath96@gmail.com>
2021-09-08 22:40:39 +0530

    using useEffect with redux

--- redux-again/src/App.js
+++ redux-again/src/App.js
@@ -1,12 +1,20 @@
-import { useSelector } from 'react-redux';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 function App() {
-  const showCart = useSelector((state )=> state.ui.isVisible)
+  const showCart = useSelector((state) => state.ui.isVisible);
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_FIREBASE_URL}`,
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
   return (
     <Layout>
-     {showCart && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
