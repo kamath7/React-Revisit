37748ec51e38f745b8c30b3c9225dcba788890bb
kamath7 <adithyakamath96@gmail.com>
2021-09-08 22:51:24 +0530

    error handling

--- redux-again/src/App.js
+++ redux-again/src/App.js
@@ -1,16 +1,48 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/cart-slice";
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.isVisible);
   const cart = useSelector((state) => state.cart);
+  c;
   useEffect(() => {
-    fetch(
-      `${process.env.REACT_APP_FIREBASE_URL}`,
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Loading...",
+          title: "Loading Data...",
+          message: "Talking with the server!",
+        })
+      );
+      const response = await fetch(`${process.env.REACT_APP_FIREBASE_URL}`, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error sending!");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "Success!",
+          title: "Success!",
+          message: "Cart data sent successfully!",
+        })
+      );
+    };
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Error",
+          title: "Error!",
+          message: "Cart data failed to send",
+        })
+      );
+    });
   }, [cart]);
   return (
     <Layout>
