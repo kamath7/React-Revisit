8bb01d68bded8b85ad5015a936912b78ec8e8922
kamath7 <adithyakamath96@gmail.com>
2021-09-10 00:54:20 +0530

    getting params

--- react-router-SPA/src/components/ProductDetail.js
+++ react-router-SPA/src/components/ProductDetail.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 const ProductDetail = () => {
+    const params = useParams()
+    console.log(params.productId)
     return (
         <section>
-            <h1>Product Details</h1>
+            <h1>Product Details for {params.productId}</h1>
         </section>
     )
 }
