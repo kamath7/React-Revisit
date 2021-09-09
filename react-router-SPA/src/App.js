f82b87228321ff79d2bd0d834bb8736a6143343d
kamath7 <adithyakamath96@gmail.com>
2021-09-10 00:29:44 +0530

    creating a header

--- react-router-SPA/src/App.js
+++ react-router-SPA/src/App.js
@@ -1,13 +1,17 @@
 import { Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import SomeProducts from "./components/SomeProducts";
+import Header from "./components/Header";
 function App() {
   return (
     <div>
-      <Route path="/welcome" component={Welcome} />
-      <Route path="/products">
-        <SomeProducts />
-      </Route>
+      <Header />
+      <main>
+        <Route path="/welcome" component={Welcome} />
+        <Route path="/products">
+          <SomeProducts />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ react-router-SPA/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+          <li>
+              <a href="/welcome">Welcome</a>
+              <a href="/products">Products</a>
+          </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
