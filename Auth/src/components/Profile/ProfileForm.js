e2f695caeade5a3241b621fee3de1e83bcfd2524
kamath7 <adithyakamath96@gmail.com>
2021-09-16 18:28:54 +0530

    password changing works

--- Auth/src/components/Profile/ProfileForm.js
+++ Auth/src/components/Profile/ProfileForm.js
@@ -1,11 +1,34 @@
+import { useContext, useRef } from 'react';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPassRef = useRef()
+  const authCtx = useContext(AuthContext)
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const enteredNewPass = newPassRef.current.value;
+
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_FIREBASE_KEY}`, {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPass,
+        returnSecureToken: false
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res)=> {
+      console.log(res)
+    })
+
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' ref={newPassRef} minLength="7" id='new-password' />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
