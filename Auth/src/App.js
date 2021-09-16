07333847f38608c77a17cbb78f08fee3085180be
kamath7 <adithyakamath96@gmail.com>
2021-09-16 20:53:46 +0530

    protecting routes

--- Auth/src/App.js
+++ Auth/src/App.js
@@ -1,22 +1,28 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Redirect, Switch, Route } from 'react-router-dom';
+import AuthContext from './store/auth-context';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
 
 function App() {
+  const authCtx = useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>}
+        {/* using navigation guards */}
+        {authCtx.isLoggedIn && <Route path='/profile'>
           <UserProfile />
+        </Route>}
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
