ea608853889690e91d7fb04b3069d4befa0f0bc3
kamath7 <adithyakamath96@gmail.com>
2021-09-17 01:35:16 +0530

    extracting params

--- next-js-rewind/sample-meetups/pages/news/[newsId].js
+++ next-js-rewind/sample-meetups/pages/news/[newsId].js
@@ -1,5 +1,11 @@
+//using special hook to extract
+
+import { useRouter } from "next/router";
+
 function DetailPage() {
-    return <h1>The important page for an ID</h1>
+  const router = useRouter();
+  ; //newsId property name
+  return <h1>The important page for an ID. {router.query.newsId}</h1>;
 }
 
-export default DetailPage
+export default DetailPage;
