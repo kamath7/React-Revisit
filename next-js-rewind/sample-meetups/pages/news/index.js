63ac1688d27a3f9a6d9d836a8e88a7b00fc541b1
kamath7 <adithyakamath96@gmail.com>
2021-09-17 01:39:56 +0530

    routing using href

--- next-js-rewind/sample-meetups/pages/news/index.js
+++ next-js-rewind/sample-meetups/pages/news/index.js
@@ -1,7 +1,18 @@
 //something.com/news
 
+import React from "react";
+
 function NewsPage() {
-    return <h1>The news page</h1>
+  return (
+    <React.Fragment>
+      <h1>The news page</h1>
+      <ul>
+          <li> <a href="/news/learnOnNextJs">Learn on Next JS</a></li>
+          <li>Learn on React JS</li>
+      </ul>
+      
+    </React.Fragment>
+  );
 }
 
-export default NewsPage
+export default NewsPage;
