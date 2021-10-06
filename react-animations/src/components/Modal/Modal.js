1ff9fe183d6ab5796d7b53ae7d21a08a60ffb538
kamath7 <adithyakamath96@gmail.com>
2021-10-06 20:21:58 +0530

    CSS transitions now used in modals

--- react-animations/src/components/Modal/Modal.js
+++ react-animations/src/components/Modal/Modal.js
@@ -1,39 +1,29 @@
 import React from "react";
 
 import "./Modal.css";
-import { Transition } from "react-transition-group";
-
+import { CSSTransition } from "react-transition-group";
+// import CSSTransition from "react-transition-group/CSSTransition";
 const animationTiming = {
   enter: 400,
   exit: 1000,
 };
 const modal = (props) => {
-  return (
-    <Transition
+    const cssClass = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+    return (
+    <CSSTransition
       in={props.show}
       mountOnEnter
       unmountOnExit
       timeout={animationTiming}
+      classNames="fade-slide"
     >
-      {(state) => {
-        const cssClass = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClass.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+      <div className={"Modal"}>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
