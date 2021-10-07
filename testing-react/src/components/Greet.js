9d8aab146dc039a09358b423d12ec2238b6a6296
kamath7 <adithyakamath96@gmail.com>
2021-10-07 20:54:09 +0530

    testing connected comps

--- testing-react/src/components/Greet.js
+++ testing-react/src/components/Greet.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import Output from './Output'
 const Greet = () => {
     const [ changeText, setChangeText] = useState(false)
 
@@ -9,7 +9,7 @@ const Greet = () => {
     return (
         <div>
             <h2>Hello Kams!</h2>
-           { changeText?  <p>Something cool</p> : <p>Will we win the IPL trophy this year?</p>}
+           { changeText?  <Output>Something cool</Output> : <Output>Will we win the IPL trophy this year?</Output>}
             <button onClick={changeTextHandler} >Changes text</button>
         </div>
     )
