def4bad58230607f3290ecee26426b734142a081
kamath7 <adithyakamath96@gmail.com>
2021-10-08 00:59:15 +0530

    mocking functions

--- testing-react/src/components/Async.test.js
+++ testing-react/src/components/Async.test.js
@@ -2,7 +2,11 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component testing", () => {
-  test("renders posts from api", async() => {
+  test("renders posts from api", async () => {
+    window.fetch = jest.fn(); //mocking the fetch api
+    window.fetch.mockResolvedValueOnce({
+        json: async ()=>[{id:'p1', title: 'first post'}]
+    });
     render(<Async />);
     const listItemElems = await screen.findAllByRole("listitem"); //this returns a promise
     expect(listItemElems).not.toHaveLength(0);
