cd99b04c2fdc87851d9fb5fa0cf4e6dd5ae7160d
kamath7 <adithyakamath96@gmail.com>
2021-10-08 19:33:10 +0530

    priming with basics

---
+++ learning-typescript-1/basics.ts
@@ -0,0 +1,10 @@
+let age: number = 24; //now you can't declar number ='24'
+
+let userName: string;
+
+userName = "Kams";
+
+let isPriv: boolean;
+isPriv = true;
+
+let hobbies:  null ; //don't really do this
