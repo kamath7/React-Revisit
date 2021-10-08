5d3cd87748af406d586b7b579f82d6cfe60911a6
kamath7 <adithyakamath96@gmail.com>
2021-10-08 19:55:00 +0530

    objects and arrays

--- learning-typescript-1/basics.ts
+++ learning-typescript-1/basics.ts
@@ -7,4 +7,25 @@ userName = "Kams";
 let isPriv: boolean;
 isPriv = true;
 
-let hobbies:  null ; //don't really do this
+let hobbies: null; //don't really do this
+
+let food: string[];
+food = ["Paneer ", " Naan"];
+
+let foo: any; //hehe why don't i use js instead
+
+foo = {
+  a: "good",
+  b: 1,
+};
+
+let bar: { a: string; b: boolean };
+bar = {
+  a: "toni kroos",
+  b: true,
+};
+
+let something: {
+    name:string,
+    b: boolean
+}[]
