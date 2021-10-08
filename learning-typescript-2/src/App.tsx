b719e41294eab9ea2278787f7de04ef6588e578f
kamath7 <adithyakamath96@gmail.com>
2021-10-09 02:43:58 +0530

    very basic ul rendering

--- learning-typescript-2/src/App.tsx
+++ learning-typescript-2/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-
+import "./App.css";
+import Todos from "./components/Todos";
 function App() {
   return (
-    <div >
-
+    <div>
+      <Todos />
     </div>
   );
 }
---
+++ learning-typescript-2/src/components/Todos.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todos() {
+  return (<div>
+      <ul>
+          <li>Eat bread</li>
+          <li>Go do potty</li>
+          <li>Sleep</li>
+      </ul>
+  </div>);
+}
+
+export default Todos;
