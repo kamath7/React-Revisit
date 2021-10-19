57bb9877c1839c4f626fa7a4c6737683ae060482
kamath7 <adithyakamath96@gmail.com>
2021-10-20 02:12:37 +0530

    creating a form

--- learning-typescript-2/src/components/NewTodo.tsx
+++ learning-typescript-2/src/components/NewTodo.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo: React.FC = () => {
+  const todoTextInp = useRef<HTMLInputElement>(null); //all dom elements have built in types
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredtxt = todoTextInp.current!.value //mentioning that we are definite of getting a val
+    if(enteredtxt.trim().length === 0){
+        return;
+    }
+    
+};
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="text">Enter your Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInp} />
 
       <button>Add Todo</button>
     </form>
